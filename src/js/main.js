48bb6bd9b8209f2b26101177698eff28e02337ff
Sean Coughlin <seanmc4@illinois.edu>
2021-09-18 14:17:27 -0500

    Add carousel and finish tables

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,20 @@
-/* Your JS here. */
-console.log('Hello World!')
+var slidePosition = 1;
+SlideShow(slidePosition);
+
+// forward/Back controls
+function plusSlides(n) {
+  SlideShow(slidePosition += n);
+}
+
+
+function SlideShow(n) {
+  var i;
+  var slides = document.getElementsByClassName("container");
+  if (n > slides.length) {slidePosition = 1}
+  if (n < 1) {slidePosition = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+
+  slides[slidePosition-1].style.display = "block";
+}
